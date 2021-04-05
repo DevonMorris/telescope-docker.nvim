3c974965cb99edffd7e41952989e17df0e037690
Devon Morris <devonmorris1992@gmail.com>
2021-04-04 23:12:58 -0400

    less hacky way of handling stderr stdout

--- lua/telescope/_extensions/docker/utils.lua
+++ lua/telescope/_extensions/docker/utils.lua
@@ -12,31 +12,26 @@ dutils.job_maker = function(cmd, bufnr, opts)
   -- if any of them are missing, cache will be skipped
   if opts.bufname ~= opts.value or not opts.bufname or not opts.value then
     local command = table.remove(cmd, 1)
+    local output = {}
     Job:new({
       command = command,
       args = cmd,
       env = opts.env,
       cwd = opts.cwd,
-      on_start = function(j)
-        j._stdout_stderr_result = {}
-        j._stdout_stderr_idx = 1
+      on_stderr = function(_, data)
+        table.insert(output, data)
       end,
-      on_stderr = function(err, result_line, j)
-        j._stdout_stderr_result[j._stdout_stderr_idx] = result_line
-        j._stdout_stderr_idx = j._stdout_stderr_idx + 1
-      end,
-      on_stdout = function(err, result_line, j)
-        j._stdout_stderr_result[j._stdout_stderr_idx] = result_line
-        j._stdout_stderr_idx = j._stdout_stderr_idx + 1
+      on_stdout = function(_, data)
+        table.insert(output, data)
       end,
       on_exit = vim.schedule_wrap(function(j)
         if not vim.api.nvim_buf_is_valid(bufnr) then return end
         if opts.mode == "append" then
-          vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, j._stdout_stderr_result)
+          vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, output)
         elseif opts.mode == "insert" then
-          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, j._stdout_stderr_result)
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, output)
         end
-        if opts.callback then opts.callback(bufnr, j._stdout_stderr_result) end
+        if opts.callback then opts.callback(bufnr, output) end
       end)
     }):start()
   else
